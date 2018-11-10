b841b2f79dff29f79009c28cee229b58aaa88911
Ben Adrian Sarmiento <benadriansarmiento@gmail.com>
2018-11-10 23:36:01 +0800

    Add feed type value object

---
+++ src/ValueObject/FeedType.php
@@ -0,0 +1,56 @@
+<?php
+
+namespace Pilmico\ValueObject;
+
+use DomainException;
+
+/**
+ * @author Ben Sarmiento <me@bensarmiento.com>
+ *
+ * @method static FeedType GAMEFOWL
+ * @method static FeedType POULTRY
+ * @method static FeedType SWINE
+ */
+final class FeedType
+{
+    const GAME_FOWL = 'game_fowl';
+    const POULTRY = 'poultry';
+    const SWINE = 'swine';
+
+    /**
+     * @var string
+     */
+    private $name;
+
+    /**
+     * @param string $name
+     */
+    private function __construct($name)
+    {
+        $constant = strtoupper($name);
+
+        if (!defined('self::' . $constant)) {
+            throw new DomainException('This feed type is invalid');
+        }
+
+        $this->name = constant('self::' . $constant);
+    }
+
+    /**
+     * @return string
+     */
+    public function getName()
+    {
+        return $this->name;
+    }
+
+    public static function __callStatic($name, $arguments)
+    {
+        return new self($name);
+    }
+
+    public function __toString()
+    {
+        return $this->getName();
+    }
+}
