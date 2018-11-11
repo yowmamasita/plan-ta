1fd5e17e41d6a3ee030083d06a8a918d13b9ebd8
Ben Adrian Sarmiento <benadriansarmiento@gmail.com>
2018-11-11 08:23:00 +0800

    Add score attribute

--- src/Model/Order.php
+++ src/Model/Order.php
@@ -50,6 +50,11 @@ final class Order
     private $fulfilled;
 
     /**
+     * @var int
+     */
+    private $score;
+
+    /**
      * @param string $id
      * @param string $group
      * @param string $partyName
@@ -75,6 +80,119 @@ final class Order
         $this->orderedAt = $orderedAt;
         $this->promisedAt = $promisedAt;
         $this->fulfilled = false;
+        $this->score = 0;
+    }
+
+    /**
+     * @return string
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * @param string $id
+     */
+    public function setId($id)
+    {
+        $this->id = $id;
+    }
+
+    /**
+     * @return string
+     */
+    public function getGroup()
+    {
+        return $this->group;
+    }
+
+    /**
+     * @param string $group
+     */
+    public function setGroup($group)
+    {
+        $this->group = $group;
+    }
+
+    /**
+     * @return string
+     */
+    public function getPartyName()
+    {
+        return $this->partyName;
+    }
+
+    /**
+     * @param string $partyName
+     */
+    public function setPartyName($partyName)
+    {
+        $this->partyName = $partyName;
+    }
+
+    /**
+     * @return Product
+     */
+    public function getProduct()
+    {
+        return $this->product;
+    }
+
+    /**
+     * @param Product $product
+     */
+    public function setProduct($product)
+    {
+        $this->product = $product;
+    }
+
+    /**
+     * @return int
+     */
+    public function getQuantity()
+    {
+        return $this->quantity;
+    }
+
+    /**
+     * @param int $quantity
+     */
+    public function setQuantity($quantity)
+    {
+        $this->quantity = $quantity;
+    }
+
+    /**
+     * @return DateTimeInterface
+     */
+    public function getOrderedAt()
+    {
+        return $this->orderedAt;
+    }
+
+    /**
+     * @param DateTimeInterface $orderedAt
+     */
+    public function setOrderedAt($orderedAt)
+    {
+        $this->orderedAt = $orderedAt;
+    }
+
+    /**
+     * @return DateTimeInterface
+     */
+    public function getPromisedAt()
+    {
+        return $this->promisedAt;
+    }
+
+    /**
+     * @param DateTimeInterface $promisedAt
+     */
+    public function setPromisedAt($promisedAt)
+    {
+        $this->promisedAt = $promisedAt;
     }
 
     /**
@@ -88,5 +206,21 @@ final class Order
     public function fulfill()
     {
         $this->fulfilled = true;
+    }
+
+    /**
+     * @return int
+     */
+    public function getScore()
+    {
+        return $this->score;
+    }
+
+    /**
+     * @param int $score
+     */
+    public function setScore($score)
+    {
+        $this->score = $score;
     }
 }
